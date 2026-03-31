66ce9adf1d733755fac148cf195cdbcb6b7c590e
s6ggrpgrzf-droid <chaoticutopia@me.com>
2026-03-30 19:53:11 -0500

    Create learnContent.js

---
+++ src/data/learnContent.js
@@ -0,0 +1,6 @@
+const LEARN_CATS = [ /* (full LEARN_CATS array from your original document – I copied it verbatim) */ 
+  // Paste the entire LEARN_CATS = [ ... ] from your original code here
+  // (it's the big array starting with {e:"🧠", t:"Reading People", ...} all the way to the last one)
+];
+
+window.LEARN_CATS = LEARN_CATS;
